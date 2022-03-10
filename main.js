2955893412d42e5c061960e7d3ef2bb375a2eef2
Richard Jackson <rpjackson2@gmail.com>
2022-03-09 21:15:18 -0600

    working on js, making filterable search input

--- main.js
+++ main.js
@@ -60,7 +60,10 @@
 //
 // let lite = document.querySelector(".light-roast")
 // console.log(lite.value)
-
+let allRoastsRadioBtns = Array.from(document.querySelectorAll(".all-roasts"));
+let coffeeOutputWrapper = document.querySelector(".coffee-output-wrapper");
+let coffeeInputSearch = document.querySelector("#coffee-input-search");
+// console.log(allRoastsRadioBtns)
 
 var coffees = [
     {id: 1, name: 'Light City', roast: 'light'},
@@ -78,3 +81,97 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+
+//SELECT COFFEE BY ROAST RADIO BUTTONS
+function selectAllRoasts(){
+    for(let i = 0; i < allRoastsRadioBtns.length;i++){
+        allRoastsRadioBtns[i].addEventListener("change",function(){
+            let checkedVal = this.value;
+            // remove all divs in the container
+            let coffeeOutputWrapperParent = document.querySelector(".coffee-output-wrapper");
+            function removeAllChildNodes(parent) {
+                while (parent.firstChild) {
+                    parent.removeChild(parent.firstChild);
+                }
+            }
+            removeAllChildNodes(coffeeOutputWrapperParent);
+            // console.log(checkedVal);
+            coffees.map((coffeeItem)=>{
+                if(coffeeItem.roast === checkedVal){
+                    createDivsAndPs(coffeeItem)
+                    //create div inside of coffee-output-wrapper
+                    // let allRoastsDiv = document.createElement("div");
+                    // allRoastsDiv.classList.add("coffee-item-output-div")
+                    // //    append it to the coffee-output-wrapper
+                    // coffeeOutputWrapper.appendChild(allRoastsDiv);
+                    // //    create p tag with coffee name
+                    // let allRoastsCoffeeNameP = document.createElement("p");
+                    // //insert coffe name
+                    // allRoastsCoffeeNameP.textContent = coffeeItem.name;
+                    // //    append it to the coffee-item-output-div/allroastsdiv
+                    // allRoastsDiv.appendChild(allRoastsCoffeeNameP);
+                    // //    create p tag with roast title
+                    // let allRoastsCoffeeRoastP = document.createElement("p");
+                    // //    add class for styling
+                    // allRoastsCoffeeRoastP.classList.add("form-text", "my-0", "sub-roast-styles")
+                    // //insert roast name
+                    // allRoastsCoffeeRoastP.textContent = coffeeItem.roast;
+                    // //    append it to the coffee-item-output-div/allroastsdiv
+                    // allRoastsDiv.appendChild(allRoastsCoffeeRoastP);
+
+                }
+
+            })
+        })
+    }
+}
+selectAllRoasts();
+
+// createDivsAndPs
+function createDivsAndPs(arrayItem){
+    let allRoastsDiv = document.createElement("div");
+    allRoastsDiv.classList.add("coffee-item-output-div")
+    //    append it to the coffee-output-wrapper
+    coffeeOutputWrapper.appendChild(allRoastsDiv);
+    //    create p tag with coffee name
+    let allRoastsCoffeeNameP = document.createElement("p");
+    //insert coffe name
+    allRoastsCoffeeNameP.textContent = arrayItem.name;
+    // allRoastsCoffeeNameP.textContent = coffeeItem.name;
+    //    append it to the coffee-item-output-div/allroastsdiv
+    allRoastsDiv.appendChild(allRoastsCoffeeNameP);
+    //    create p tag with roast title
+    let allRoastsCoffeeRoastP = document.createElement("p");
+    //    add class for styling
+    allRoastsCoffeeRoastP.classList.add("form-text", "my-0", "sub-roast-styles")
+    //insert roast name
+    allRoastsCoffeeRoastP.textContent = arrayItem.roast;
+    //    append it to the coffee-item-output-div/allroastsdiv
+    allRoastsDiv.appendChild(allRoastsCoffeeRoastP);
+}
+
+
+
+// ***** SEARCH FOR COFFEE BY TYPING IN NAME INTO INPUT *****
+function inputSearch(){
+    coffeeInputSearch.addEventListener("keyup", (e)=>{
+        e.preventDefault();
+        console.log(e.target.value);
+        let inputValue = e.target.value.toLowerCase();
+        let filteredCoffeeNames= coffees.filter((element)=>{
+return element.name.toLowerCase().includes(inputValue)});
+
+filteredCoffeeNames.forEach((element)=>{
+    createDivsAndPs(element)
+})
+// createDivsAndPs()
+//     let filteredCoffeeNames = coffees.filter((element)=>{
+//         element.name.includes(inputValue);
+//     console.log(filteredCoffeeNames);
+// })
+
+    })
+}
+inputSearch()
+
+// const coffee = coffees.filter(nescof => console.log(nescof.name) );
